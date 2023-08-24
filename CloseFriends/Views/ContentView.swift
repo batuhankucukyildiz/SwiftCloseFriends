d411e00b98dbe67f673a9b4f9d8101cb0d3683ac
Batuhan <kucukyildizbatuhan@gmail.com>
2023-08-24 22:01:17 +0300

    loading user , shimmer effect added

--- CloseFriends/Views/ContentView.swift
+++ CloseFriends/Views/ContentView.swift
@@ -6,23 +6,40 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
     @ObservedObject var users = UsersViewModel()
+    @State var isLoading = true
     
     var body: some View {
-        List(users.users) { user in
-            VStack(alignment: .leading) {
-                Text(user.name)
-                    .font(.headline)
-                Text(user.username)
-                    .font(.subheadline)
-                Text(user.email)
-                    .font(.subheadline)
+        if users.users.isEmpty && isLoading {
+            ScrollView {
+                LazyVStack(spacing: 1) {
+                    ForEach(0..<5) { _ in
+                        ContentLoader()
+                    }
+                }
+            }
+            .onAppear {
+                // Simulate loading users
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    isLoading = false
+                }
+            }
+        } else {
+            List(users.users) { user in
+                VStack(alignment: .leading) {
+                    Text(user.name)
+                        .font(.headline)
+                    Text(user.username)
+                        .font(.subheadline)
+                    Text(user.email)
+                        .font(.subheadline)
+                }
             }
         }
     }
 }
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
