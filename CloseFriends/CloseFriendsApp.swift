32b31f986f01bca1db83305e07f4f2cd62706206
Batuhan <kucukyildizbatuhan@gmail.com>
2023-08-24 21:15:17 +0300

    Initial Commit

---
+++ CloseFriends/CloseFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloseFriendsApp.swift
+//  CloseFriends
+//
+//  Created by Batuhan Küçükyıldız on 24.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CloseFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
